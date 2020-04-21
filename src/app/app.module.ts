f899ee433d4d75a786c0da63243260b6e76d6496
leasy1411 <36615741+leasy1411@users.noreply.github.com>
2020-04-21 11:05:02 +0900

    удалено все ненужное + импортирован FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms'
+
 @NgModule({
   declarations: [
     AppComponent
