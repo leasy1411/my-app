d1aa53774795b8582303668d9c132d3b93e09357
leasy1411 <36615741+leasy1411@users.noreply.github.com>
2020-04-21 11:11:17 +0900

    определен класс Product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,3 +9,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   appTitle = 'Kitty';
 }
+
+export class Product {
+  constructor(
+    public title: string,
+    public price: number,
+    public description: string
+  ) {}
+}
